6e85151519143787e97873244630a313b834213c
kauã dos Santos Silva <kauassilvagithub@gmail.com>
2023-03-28 19:44:37 -0300

    feature: wrapping image text

--- src/App.java
+++ src/App.java
@@ -22,30 +22,21 @@ public class App {
          * Fazer uma conexão HTTP e buscar os top filmes
          * ----------------------------------------
          */ 
-        // Define a URL do arquivo JSON
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        // Cria um objeto URI com a URL
         URI address = URI.create(url);
-        // Cria um cliente HTTP
         var client = HttpClient.newHttpClient();
-        // Cria uma solicitação HTTP GET com o URI definido
         var request = HttpRequest.newBuilder(address).GET().build();
-        // Envia a solicitação e obtém a resposta como uma String
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        // Armazena a String de resposta em uma variável
         String body = response.body();
-        // Imprime a String de resposta na saída padrão (console)
-        //System.out.println(body);
 
 
         /*
          * Extrair só os dados que interessam (Título, poster, classificação)
          * ----------------------------------------
          */
-        // Cria um objeto JsonParser para analisar a resposta da API e extrair os dados dos filmes
         var parser = new JsonParser();
-        // Analisa a String JSON da resposta e retorna uma lista de mapas contendo informações dos filmes
         List<Map<String, String>> movieList = parser.parse(body);
+
 
         /*
          * Exibir e manipular os dados
